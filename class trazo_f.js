79e2bc6f825b2d20e5bb687f0374545e96d44ffe
SiroBarcelone <82860216+SiroBarcelone@users.noreply.github.com>
2023-06-15 21:53:06 -0300

    Add files via upload

--- class trazo_f.js
+++ class trazo_f.js
@@ -1,7 +1,17 @@
 class Trazo_f {
-  constructor(quetrazo) {
-    // Variable para elegir el trazo
-    this.quetrazo = quetrazo;
+  constructor(imgs_trazos) {
+    this.quetrazo = imgs_trazos;
+
+    this.posy_reset = random(height + 30, height + 80); // Posición Y de reinicio
+
+    // Asegurarse de que el array imgs_trazos se haya cargado completamente
+    if (imgs_trazos.length > 0) {
+      // Realizar el procesamiento de las imágenes de trazos figura
+      for (let i = 0; i < imgs_trazos.length; i++) {
+        let img = imgs_trazos[i];
+        img.resize(80, 40); // Ajustar el tamaño de los trazos (aumentar el tamaño)
+      }
+    }
 
     // Vars movimiento
     this.vel = random(5, 20);
@@ -20,14 +30,15 @@ class Trazo_f {
     this.opacidad = 0.05;
     this.randomcol = random(200, 360);
 
-    // Fondo para obtener color
-    this.img_fondo = loadImage('imagenes/colorfondo.jpg');
-   // image(this.img_fondo, 0, 0);
+     // Variables adicionales
+    // this.posy_reset = random(height + 30, height + 80); // Posición Y de reinicio
+  }
+
+  agregarTrazo(img) {
+    this.quetrazo.push(img); // Agregar una imagen de trazo al array this.quetrazo
   }
 
   darcolor() {
-    let color = this.img_fondo.get();
-
     this.difX = abs(mouseX - pmouseX);
     this.difY = abs(mouseY - pmouseY);
     this.velmouse = floor(this.difX + this.difY);
@@ -44,52 +55,61 @@ class Trazo_f {
   }
 
   saltaralprincipio_f() {
-    this.posy = height;
+    this.posy = random(height + 30, height + 80); // Reiniciar posición Y a posy_reset
     this.randomcol = random(200, 360);
     this.posx_f = random(0, width - 50);
   }
 
   movertrazo_f() {
     if (this.posx_f > width / 2 - 100 && this.posx_f < width / 2 + 100) {
-      this.angulo = 50;
+      this.angulo = 90;
     } else {
       this.angulo = map(this.posy, height, 0, 35, 120);
     }
-
+  
     if (this.posx_f > width / 2 - 100 && this.posx_f < width / 2 + 100 && this.posy > height / 2) {
       this.angulo = map(this.posy, height, 0, 50, 120);
     }
-
+  
     this.angulo += noise(this.posy * 0.01, millis() * 0.001) * 100 - 20;
-
+  
     this.dx = cos(radians(this.angulo));
     this.dy = sin(radians(this.angulo));
-
+  
     this.posy -= this.dy * this.vel;
-
+  
     if (this.posx_f < width / 2 || (this.posx_f < width / 2 && this.dy < height / 2)) {
       this.posx_f += this.dx * this.vel;
     } else if (this.posx_f > width / 2 || (this.posx_f > width / 2 && this.dy < height / 2)) {
       this.posx_f -= this.dx * this.vel;
     }
-
+  
     if (this.posy < -80) {
       this.saltaralprincipio_f();
     }
+  
+    // Añadir espacio medio
+    this.posx_f += this.dx * this.vel * 0.1; // Ajusta el valor 0.5 según tus necesidades
   }
+  
+  
+  
 
   dibujar() {
     push();
     translate(this.posx_f, this.posy);
-    tint(this.color, 50, this.brillo, this.opacidad);
-    image(this.quetrazo, 0, 0);
-    pop();
+    rotate(radians(this.angulo + 0));
+    if (this.posx_f < width / 2) {
+      rotate(radians(this.angulo + 250));
+    }
+    let trazoIndex = frameCount % this.quetrazo.length; // Índice del trazo actual a dibujar
+    let img = this.quetrazo[trazoIndex];
+    tint(255, 30, this.brillo, this.opacidad); // Ajusta el color de la imagen
+    image(img, 20, 0); // Dibuja la imagen en la posición actual relativa a translate()
 
-    push();
-    translate(this.posx_f, this.posy);
-    rotate(radians(this.angulo + random(75, 130)));
-    tint(this.color, 30, this.brillo, this.opacidad);
-    image(this.quetrazo, 0, 0);
+    if (this.posy < -80) {
+      this.saltaralprincipio_f();
+    }
     pop();
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,67 +1,65 @@
-
 // Array de objetos Trazo_f
 let tfon = [];
 // Array de objetos trazo_fig
 let tfig = [];
-// Mascara figura
+// Máscara figura
 let mascarafigura;
 // Array de imágenes de trazos
 let imgs_trazos = [];
+// Declarar una variable global para la máscara de fondo
+//let mascarafondo;
 
 // Carga de recursos antes de iniciar el sketch
 function preload() {
-  // Imagen simil lienzo
-  imgfondo = loadImage('imagenes/lienzofondo2.jpg');
-  // Trazo del fondo
-  trazofondo = loadImage('trazos/trazofondo_13.png');
-  // Mascara fondo
-  mascaratfondo = loadImage('trazos/trazofondo_14_a.png');
-
-  // Recursos figura
-  // URLs de las imágenes de trazo figura
-  let urls = [
-    "trazos/trazofondo_01.png",
-    "trazos/trazofondo_02.png",
-    "trazos/trazofondo_03.png",
-    "trazos/trazofondo_04.png",
-    "trazos/trazofondo_05.png",
-    "trazos/trazofondo_06.png",
-    "trazos/trazofondo_07.png",
-    "trazos/trazofondo_08.png",
-    "trazos/trazofondo_09.png"
-  ];
-
   // Carga de la máscara figura
   mascarafigura = loadImage('trazos/mascara_figura3.jpg');
 
- // Carga de las imágenes de trazos figura en el array imgs_trazos
-for (let i = 0; i < urls.length; i++) {
-  // hay que crear una imagen create img, en cada vuelta del ciclo y usar de mascara imgs_trazos
-  let img = loadImage(urls[i],() => {
-    //la mascara es media al pedo si la img ya es transparente
-    imgs_trazos[i].filter(INVERT); // Aplicar el filtro invert a la imagen en la posición i del array
-    imgs_trazos[i].mask(imgs_trazos[i]); // Enmascarar el trazo con la misma imagen del trazo
-    //se puede aplicar un resize o hacer la mascara con un circulo o hacer las imagenes mas chicas//
-    //imgs_trazos[i].resize(20,20);
-  });
-  imgs_trazos.push(img);
-}
+  // Carga de la máscara de fondo
+  //mascarafondo = loadImage('trazos/trazosfondo/trazofondo_12.png');
 
+  // Resto de la carga de recursos...
+
+  // URLs de las imágenes de trazos fondo
+  let urls = [
+    "trazos/trazosfondo/trazofondo_01.png",
+    "trazos/trazosfondo/trazofondo_02.png",
+    "trazos/trazosfondo/trazofondo_03.png",
+    "trazos/trazosfondo/trazofondo_04.png",
+    "trazos/trazosfondo/trazofondo_05.png",
+    "trazos/trazosfondo/trazofondo_06.png",
+    "trazos/trazosfondo/trazofondo_07.png",
+    "trazos/trazosfondo/trazofondo_08.png",
+    "trazos/trazosfondo/trazofondo_09.png",
+    "trazos/trazosfondo/trazofondo_10.png",
+    "trazos/trazosfondo/trazofondo_11.png",
+    "trazos/trazosfondo/trazofondo_12.png",
+    "trazos/trazosfondo/trazofondo_13.png",
+    "trazos/trazosfondo/trazofondo_14.png"
+  ];
+
+  // Carga de las imágenes de trazos figura en el array imgs_trazos
+  for (let i = 0; i < urls.length; i++) {
+    let img = loadImage(urls[i], () => {
+      // Aplicar la máscara de fondo a la imagen de trazo
+     // img.mask(mascarafondo);
+      imgs_trazos.push(img);
+    });
+  }
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
   // Fondo
-  trazofondo.resize(trazofondo.width / 2, trazofondo.height / 2);
-  trazofondo.mask(mascaratfondo);
+  //trazofondo.resize(trazofondo.width / 2, trazofondo.height / 2);
+  //trazofondo.mask(mascaratfondo);
   colorMode(HSB);
 
-  // Objetos Trazo_f
-  for (let i = 0; i < 10; i++) {
-    let trazo_f = new Trazo_f(trazofondo);
-    tfon.push(trazo_f);
-  }
+// Crear objetos Trazo_f después de cargar las imágenes de trazos de fondo
+for (let i = 0; i < 20; i++) {
+  let trazo_f = new Trazo_f(imgs_trazos);
+  tfon.push(trazo_f);
+}
 
   // Objetos trazo_fig
   for (let j = 0; j < 10; j++) {
